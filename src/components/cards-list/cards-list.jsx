3e1fd214f4dd26e51809fbb9b931398646a37e23
Otkrovennost <Otkrovennost@mail.ru>
2020-07-03 01:12:51 +0300

    удаляет лишнее

--- src/components/cards-list/cards-list.jsx
+++ src/components/cards-list/cards-list.jsx
@@ -20,32 +20,6 @@ const CardsList = ({offers, onTitleClick, onCardHover, cardClass}) => {
   );
 };
 
-// class CardsList extends PureComponent {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     const {offers, onTitleClick, onCardHover, cardClass} = this.props;
-
-//     const cardsListName = cardClass === `cities` ? `cities__places-list places__list tabs__content` : `near-places__list places__list`;
-
-//     return (
-//       <div className={cardsListName}>
-//         {offers.map((offer) =>
-//           <Card
-//             offer={offer}
-//             key={offer.id}
-//             onTitleClick={onTitleClick}
-//             onCardHover={onCardHover}
-//             cardClass={cardClass}
-//           />
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
 CardsList.propTypes = {
   offers: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
